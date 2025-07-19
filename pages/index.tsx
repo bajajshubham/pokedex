441f18e6f1009a65fc228b42349b741f52cd42fa
Shubham Bajaj <shubham2bajaj@gmail.com>
2025-07-19 19:44:40 +0200

    added pagination for pokemon table

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import PokemonTable from "@/components/PokemonTable"
 import { InferGetServerSidePropsType } from "next"
+import {useRouter} from "next/router"
 
 type Pokemon = {
   name: string
@@ -13,16 +14,29 @@ interface Props {
   results: Pokemon[]
 }
 
-export default function Page({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data)
+export default function Page({ pokemons, totalCount, page, }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter()
+
+  function goToPage(newPage: number) {
+    router.push(`/?page=${newPage}`)
+  }
   // Render data...
   return (
     <div className="p-5">
       <h1>Pokédex</h1>
-      <PokemonTable data={data.results} />
+      <PokemonTable data={pokemons} />
+
+
+      <div className="mt-5">
+        <button onClick={() => goToPage(page - 1)} disabled={page <= 1}>
+          Previous
+        </button>
+        <span className="mx-2.5">Page {page}</span>
+        <button onClick={() => goToPage(page + 1)} disabled={page * 20 >= totalCount}>
+          Next
+        </button>
+      </div>
     </div>
-
-
   )
 }
 
@@ -36,5 +50,11 @@ export async function getServerSideProps(context: any) {
   const data: Props = await res.json()
 
   // Pass data to the page via props
-  return { props: { data } }
+  return {
+    props: {
+      pokemons: data.results,
+      totalCount: data.count,
+      page,
+    }
+  }
 }
